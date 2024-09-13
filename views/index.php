acb3cc5cbe56aecfd42b5adf25d3019109e9e531
newoutsider <90830312+negativexp@users.noreply.github.com>
2024-09-13 09:09:41 +0200

    dik dik

---
+++ views/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/resources/style.css">
+    <title>StrikeMaster</title>
+</head>
+<body>
+<header>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="/resources/logo.png">
+        </div>
+        <nav>
+            <a>Rezervace</a>
+            <a>Kontakt</a>
+            <a>Admin</a>
+        </nav>
+    </div>
+</header>
+<main>
+    <section>
+        <div class="w50">
+            <h1>Strike Master</h1>
+            <p>Vítejte v našem moderním bowlingovém centru, které nabízí víc než jen hru. Spojte radost ze hry s vynikajícím jídlem a pitím v naší stylové restauraci. Ať už hledáte místo pro rodinný výlet, večer s přáteli nebo firemní akci, StrikeMaster je ideální destinací. Přijďte si užít atmosféru plnou soutěžení, smíchu a skvělých zážitků!</p>
+        </div>
+        <div class="w50">
+            <img src="/../resources/kuzelky.png">
+        </div>
+    </section>
+    <section>
+        <div class="w70">
+            <h1>Strike Master</h1>
+            <p>Vítejte v našem moderním bowlingovém centru, které nabízí víc než jen hru. Spojte radost ze hry s vynikajícím jídlem a pitím v naší stylové restauraci. Ať už hledáte místo pro rodinný výlet, večer s přáteli nebo firemní akci, StrikeMaster je ideální destinací. Přijďte si užít atmosféru plnou soutěžení, smíchu a skvělých zážitků!</p>
+        </div>
+        <div class="w30">
+            <h1>Strike Master</h1>
+            <p>Vítejte v našem moderním bowlingovém centru, které nabízí víc než jen hru. Spojte radost ze hry s vynikajícím jídlem a pitím v naší stylové restauraci. Ať už hledáte místo pro rodinný výlet, večer s přáteli nebo firemní akci, StrikeMaster je ideální destinací. Přijďte si užít atmosféru plnou soutěžení, smíchu a skvělých zážitků!</p>
+        </div>
+    </section>
+</main>
+<footer>
+    mrdko
+</footer>
+</body>
+</html>
