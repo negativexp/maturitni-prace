33f4201ac75a5df15b97425c399a96f06526a59a
newoutsider <90830312+negativexp@users.noreply.github.com>
2024-09-14 16:53:08 +0200

    swag

--- views/index.php
+++ views/index.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/resources/style.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <title>StrikeMaster</title>
 </head>
 <body class="hp">
@@ -15,7 +16,7 @@
             <img alt="Bowlingové kuželky" title="Bowlingové kuželky" src="/resources/kuzelky.png">
         </div>
         <nav>
-            <a>Rezervace</a>
+            <a href="#rezervace">Rezervace</a>
             <a>Kontakt</a>
             <a>Admin</a>
         </nav>
@@ -60,8 +61,8 @@
             <p><span class="bold">Neděle</span>: ZAVŘENO</p>
         </div>
     </section>
-    <section>
-        <div class="w100">
+    <section id="rezervace">
+        <div class="w100 tacen">
             <h2>Vytvořte si rezervaci</h2>
             <form>
                 <div class="page" id="time">
@@ -139,6 +140,7 @@
                     <div id="calculation">
                         <p id="price">Cena: 120 Kč</p>
                     </div>
+                    <div class="g-recaptcha" data-sitekey="YOUR_SITE_KEY"></div>
                     <button type="submit">Odeslat rezervaci</button>
                 </div>
                 <div class="arrows">
@@ -150,6 +152,51 @@
                     </div>
                 </div>
             </form>
+        </div>
+    </section>
+    <section>
+        <div class="w100">
+            <hr>
+        </div>
+    </section>
+    <section>
+        <div class="w50">
+            <h2>Jídelní lístek</h2>
+        </div>
+        <div class="w50">
+            <h2>Proč Strike Master?</h2>
+            <ul>
+                <li>
+                    <h3>Zábava pro všechny generace</h3>
+                    <p>Strike Master je místo, kde se skvěle baví jak děti, tak dospělí. Naše moderní bowlingové dráhy jsou ideální pro rodinné výlety, oslavy narozenin nebo jen tak na večer s přáteli. Ať už jste začátečník nebo zkušený hráč, u nás si užijete hru na maximum.</p>
+                </li>
+                <li>
+                    <h3>Stylové prostředí</h3>
+                    <p>Naše centrum vás okouzlí svým moderním a útulným designem. Po hře si můžete odpočinout v pohodlném prostředí naší restaurace nebo baru, kde si vychutnáte výborné jídlo a nápoje.</p>
+                </li>
+                <li>
+                    <h3>Vynikající kuchyně</h3>
+                    <p>Kromě skvělé hry nabízíme i lahodné občerstvení. V naší restauraci najdete vše od drobných předkrmů po výtečné hlavní chody a domácí dezerty. Nezáleží na tom, jestli máte chuť na burger, salát nebo něco sladkého – u nás si vybere každý.</p>
+                </li>
+                <li>
+                    <h3>Perfektní místo pro oslavy a akce</h3>
+                    <p>Hledáte místo pro narozeninovou oslavu, firemní večírek nebo přátelský turnaj? Strike Master je ideálním místem pro jakoukoliv společenskou událost. Rádi vám připravíme speciální balíčky, které zahrnují hru, občerstvení i zábavu na míru.</p>
+                </li>
+                <li>
+                    <h3>Dlouhé hodiny zábavy</h3>
+                    <p>U nás se nudit nebudete! Pokud plánujete strávit na bowlingové dráze více než 5 hodin, je třeba se předem domluvit s majitelem, aby bylo zajištěno, že vám nic nebrání v tom užít si celou dobu plnou zábavy a soutěžení.</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <section>
+        <div class="w100">
+            <h2>Zábava pro každého</h2>
+            <p>Strike Master je víc než jen obyčejné bowlingové centrum – je to místo, kde se každý stává šampionem a kde každá návštěva znamená nové zážitky. Ať už přicházíte s rodinou, přáteli nebo kolegy z práce, u nás si užijete skvělý čas plný smíchu a soutěžení. Naše moderní bowlingové dráhy zaručují skvělou hru pro hráče všech úrovní, zatímco naše restaurace nabízí delikátní pokrmy, které vás potěší po každém vítězném hodu.</p>
+            <p>&nbsp;</p>
+            <p>Nezapomeňte, pokud máte v plánu strávit na bowlingové dráze <span class="bold">více než 5 hodin</span>, je nutné se předem <span class="bold">domluvit</span> s <span class="bold">majitelem</span>. Chceme, aby každý host měl možnost si svůj čas u nás maximálně užít!</p>
+            <p>&nbsp;</p>
+            <p>Strike Master je také skvělým místem pro pořádání narozeninových oslav, firemních akcí nebo přátelských turnajů. Máme pro vás připravené speciální balíčky, které kombinují hru, občerstvení a mnoho zábavy v jednom.</p>
         </div>
     </section>
 </main>
